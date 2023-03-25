262ade4b0442c7949772bcfee0813320e835aed7
ArjunRAj77 <arjunraj.pala@gmail.com>
2023-03-25 23:24:11 +0530

    Adding more emojis

--- pages/Standards.py
+++ pages/Standards.py
@@ -60,23 +60,25 @@ df = pd.DataFrame(data)
 
 def color_table(val):
 
-    if val == 'Suitable':
+    if val == 'Suitable' or val == 'Excellent' or val == 'Good' or val=='Fair' :
         color = 'green'
-    elif val == 'Danger':
+    elif val == 'Danger' or val == 'Unacceptable' or val == 'Very Poor' :
         color = 'red'
-    elif val == 'Permissible limit':
-        color = 'yellow'
+    elif val == 'Permissible limit' or val == 'Poor':
+        color = 'orange'
     elif val == 'Not recommended':
-        color = 'yellow'
+        color = 'orange'
     else:
         color = 'black'
     return f'color: {color}'
 
 # Define the Streamlit app
 def app():
-    st.title('Water Quality Standards ✔')
-    st.subheader("TDS Level Chart for Drinking Water")
-    tds_expander = st.expander("🌌 Click Here!")
+    st.title('Water Quality Standards :droplet: ')
+    st.subheader(" ✔ Level Charts for Drinking Water")
+    st.write("Below contains various standards for drinking water as per the Indian Standards for Safe Drinking Water.")
+    st.subheader(":diamond_shape_with_a_dot_inside: TDS [Total Dissolved solids]")
+    tds_expander = st.expander(":snowflake: More info")
     with tds_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
@@ -88,78 +90,78 @@ def app():
         st.write(df_table)
         # Display tables
 
-    st.subheader("Fluoride")
-    fluoride_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Fluoride")
+    fluoride_expander = st.expander(":snowflake: More info")
     with fluoride_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
-        st.write("Fluoridation of water is a controlled adjustment of fluoride to the public water supply to reduce tooth decay. Fluoride-containing water contains fluoride at a level that is effective at blocking cavities; This can occur naturally or by adding fluoride.")
+        st.write("Fluoridation of water is a controlled adjustment of fluoride to the public water supply to reduce tooth decay.\n\n Fluoride-containing water contains fluoride at a level that is effective at blocking cavities; This can occur naturally or by adding fluoride.")
       with col2:
         fluoride_table = fluoride.style.applymap(color_table, subset=['Suitability'])
         st.write(fluoride_table)
 
-    st.subheader("Arsenic")
-    arsenic_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Arsenic")
+    arsenic_expander = st.expander(":snowflake: More info")
     with arsenic_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
-        st.write("Arsenic is a naturally occurring element found in soil and rock, and can be present in water sources. It is considered a toxic substance that can cause various health issues such as skin lesions, cancer, and cardiovascular disease when consumed at high levels over long periods of time.")
+        st.write("Arsenic is a naturally occurring element found in soil and rock, and can be present in water sources.\n\n It is considered a toxic substance that can cause various health issues such as skin lesions, cancer, and cardiovascular disease when consumed at high levels over long periods of time.")
       with col2:
         arsenic_table = arsenic.style.applymap(color_table, subset=['Suitability'])
         st.write(arsenic_table)
 
-    st.subheader("Iron")
-    iron_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Iron")
+    iron_expander = st.expander(":snowflake: More info")
     with iron_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
-        st.write("Iron is typically measured as part of the assessment of the aesthetic quality of the water, as it can cause discoloration, staining, and a metallic taste or odor. Excessive levels of iron in water can also lead to clogging of pipes and fixtures, reduced water flow, and damage to water treatment equipment. ")
+        st.write("Iron is typically measured as part of the assessment of the aesthetic quality of the water, as it can cause discoloration, staining, and a metallic taste or odor.\n\n Excessive levels of iron in water can also lead to clogging of pipes and fixtures, reduced water flow, and damage to water treatment equipment. ")
       with col2:
         iron_table = iron.style.applymap(color_table, subset=['Suitability'])
         st.write(iron_table)
 
-    st.subheader("Nitrate")
-    nitrate_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Nitrate")
+    nitrate_expander = st.expander(":snowflake: More info")
     with nitrate_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
-        st.write("Nitrate is a compound that is formed naturally when nitrogen combines with oxygen or ozone. A high concentration of nitrate in well water often results in good construction, well location, and overuse of chemical fertilizers, or improper disposal of human and animal waste.")
+        st.write("Nitrate is a compound that is formed naturally when nitrogen combines with oxygen or ozone. \n\nA high concentration of nitrate in well water often results in good construction, well location, and overuse of chemical fertilizers, or improper disposal of human and animal waste.")
       with col2:
         nitrate_table = nitrate.style.applymap(color_table, subset=['Suitability'])
         st.write(nitrate_table)
 
-    st.subheader("pH")
-    ph_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: pH")
+    ph_expander = st.expander(":snowflake: More info")
     with ph_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
-        st.write("PH Value indicates the hydrogen-ion concentration of a solution. In the form of acids and the bases in the solution dissociate to produce hydrogen ions [H +] And hydroxyl ions [OH-] Respectively, pH is used to indicate the intensity of an acidic or alkaline state.")
+        st.write("PH Value indicates the hydrogen-ion concentration of a solution. \n\nIn the form of acids and the bases in the solution dissociate to produce hydrogen ions [H +] And hydroxyl ions [OH-] Respectively, pH is used to indicate the intensity of an acidic or alkaline state.")
       with col2:
         ph_table = ph.style.applymap(color_table, subset=['Suitability'])
         st.write(ph_table)
 
-    st.subheader("Chloride")
-    chloride_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Chloride")
+    chloride_expander = st.expander(":snowflake: More info")
     with chloride_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
-        st.write("High levels of chloride in drinking water can affect its taste and may cause health concerns for people on low-sodium diets. In addition, elevated chloride levels in freshwater bodies can be harmful to aquatic organisms and can contribute to the degradation of freshwater ecosystems.")
+        st.write("High levels of chloride in drinking water can affect its taste and may cause health concerns for people on low-sodium diets. \n\nIn addition, elevated chloride levels in freshwater bodies can be harmful to aquatic organisms and can contribute to the degradation of freshwater ecosystems.")
       with col2:
         chloride_table = chloride.style.applymap(color_table, subset=['Suitability'])
         st.write(chloride_table)
 
-    st.subheader("Lead")
-    lead_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Lead")
+    lead_expander = st.expander(":snowflake: More info")
     with lead_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
-        st.write("lead is a heavy metal that can be found in drinking water sources. It can be harmful to human health if ingested in high levels over time. Lead is often introduced into water systems through old pipes and plumbing fixtures, as well as from industrial activities and pollution.")
+        st.write("lead is a heavy metal that can be found in drinking water sources. It can be harmful to human health if ingested in high levels over time.\n\n Lead is often introduced into water systems through old pipes and plumbing fixtures, as well as from industrial activities and pollution.")
       with col2:
         lead_table = lead.style.applymap(color_table, subset=['Suitability'])
         st.write(lead_table)
 
-    st.subheader("Zinc")
-    zinc_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Zinc")
+    zinc_expander = st.expander(":snowflake: More info")
     with zinc_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
@@ -168,18 +170,18 @@ def app():
         zinc_table = zinc.style.applymap(color_table, subset=['Suitability'])
         st.write(zinc_table)
 
-    st.subheader("Copper")
-    copper_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Copper")
+    copper_expander = st.expander(":snowflake: More info")
     with copper_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
-        st.write("Copper is a common contaminant that can affect the quality of water. It can enter water sources through industrial processes, plumbing systems, and natural deposits. In high concentrations, copper can cause a metallic taste in water and can be toxic to humans and aquatic organisms.")
+        st.write("Copper is a common contaminant that can affect the quality of water. It can enter water sources through industrial processes, plumbing systems, and natural deposits.\n\n In high concentrations, copper can cause a metallic taste in water and can be toxic to humans and aquatic organisms.")
       with col2:
         copper_table = copper.style.applymap(color_table, subset=['Suitability'])
         st.write(copper_table)
 
-    st.subheader("Sulphate")
-    sulphate_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Sulphate")
+    sulphate_expander = st.expander(":snowflake: More info")
     with sulphate_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
@@ -188,8 +190,8 @@ def app():
         sulphate_table = sulphate.style.applymap(color_table, subset=['Suitability'])
         st.write(sulphate_table)
 
-    st.subheader("Chlorine")
-    chlorine_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Chlorine")
+    chlorine_expander = st.expander(":snowflake: More info")
     with chlorine_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
@@ -198,12 +200,12 @@ def app():
         chlorine_table = chlorine.style.applymap(color_table, subset=['Suitability'])
         st.write(chlorine_table)
 
-    st.subheader("Manganese")
-    manganese_expander = st.expander("🌌 Click Here!")
+    st.subheader(":diamond_shape_with_a_dot_inside: Manganese")
+    manganese_expander = st.expander(":snowflake: More info")
     with manganese_expander:
       col1, col2 = st.columns([2, 2])
       with col1:
-        st.write("Manganese is a naturally occurring element that can be found in rocks, soil, water, and air. In water quality analysis, manganese is an important parameter to measure as it can affect the taste, color, and odor of water. High levels of manganese in drinking water can also be harmful to human health, particularly for people with liver and kidney disorders.")
+        st.write("Manganese is a naturally occurring element that can be found in rocks, soil, water, and air.\n\n In water quality analysis, manganese is an important parameter to measure as it can affect the taste, color, and odor of water.\n\n High levels of manganese in drinking water can also be harmful to human health, particularly for people with liver and kidney disorders.")
       with col2:
         manganese_table = manganese.style.applymap(color_table, subset=['Suitability'])
         st.write(manganese_table)
